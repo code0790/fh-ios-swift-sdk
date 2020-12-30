d12f273db982da92b4144b02ec6624aa4400c3d1
Navpreet Singh <Apple@Macbook>
2020-12-30 14:05:06 +0530

    Support for min ios 13

--- FeedHenry/OAuth/OAuthViewController.swift
+++ FeedHenry/OAuth/OAuthViewController.swift
@@ -17,7 +17,10 @@
 /**
  Present a customized UIWebView to perform OAuth authentication.
  */
-open class OAuthViewController: UIViewController, UIWebViewDelegate {
+
+import WebKit
+
+open class OAuthViewController: UIViewController, WKNavigationDelegate {
     var url: URL?
     var completionHandler: (Response, NSError?) -> Void = { (respo:Response, err:NSError?) -> Void in
     }
@@ -27,29 +30,43 @@ open class OAuthViewController: UIViewController, UIWebViewDelegate {
     /// Override standard method to make the view full screen.
     override open func viewDidLoad() {
         super.viewDidLoad()
-        let webView:UIWebView = UIWebView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+        let webView:WKWebView = WKWebView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
         guard let url = url else {return}
-        webView.loadRequest(URLRequest(url: url))
-        webView.delegate = self;
+        webView.load(URLRequest(url: url))
+        webView.navigationDelegate = self;
         self.view.addSubview(webView)
     }
     
     /// Override to deal with error.
-    open func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+//    open func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+//        print("Webview fail with error \(error)");
+//    }
+    
+    open func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
         print("Webview fail with error \(error)");
     }
     
     /// Override to retrieve auth token and store it.
-    open func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebView.NavigationType) -> Bool {
-        print("Start to load url: \(String(describing: request.url))")
+//    open func webView(_ webView: UIWebView, shouldStartLoadWith request: URLRequest, navigationType: UIWebView.NavigationType) -> Bool {
+//        print("Start to load url: \(String(describing: request.url))")
+//        authInfo = [:]
+//        do {
+//            authInfo = try processQuery(request.url?.query)
+//        } catch {
+//            print("OAuthViewController: an error occurs reading authResponse from cloud app.")
+//            return false
+//        }
+//        return true;
+//    }
+    
+    open func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        print("Start to load url: \(String(describing: webView.url))")
         authInfo = [:]
         do {
-            authInfo = try processQuery(request.url?.query)
+            authInfo = try processQuery(webView.url?.query)
         } catch {
             print("OAuthViewController: an error occurs reading authResponse from cloud app.")
-            return false
         }
-        return true;
     }
     
     func processQuery(_ query: String?) throws -> [String: AnyObject]? {
@@ -75,12 +92,24 @@ open class OAuthViewController: UIViewController, UIWebViewDelegate {
     }
     
     /// Override for logging purpose.
-    open func webViewDidStartLoad(_ webView: UIWebView) {
+//    open func webViewDidStartLoad(_ webView: UIWebView) {
+//        print("Webview started Loading")
+//    }
+    
+    open func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
         print("Webview started Loading")
     }
     
     /// Override to close the view on success.
-    open func webViewDidFinishLoad(_ webView: UIWebView) {
+//    open func webViewDidFinishLoad(_ webView: UIWebView) {
+//        print("Webview did finish load")
+//        if isFinished {
+//            isFinished = false
+//            closeView()
+//        }
+//    }
+    
+    open func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         print("Webview did finish load")
         if isFinished {
             isFinished = false
